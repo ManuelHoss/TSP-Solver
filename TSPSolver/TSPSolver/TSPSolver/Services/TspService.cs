62f295539e5c318e04732bfce988cf869df64b67
Manuel Hoss <manuel.hoss@hotmail.de>
2017-02-08 01:03:16 +0100

    Parsing from AdjacencyMatrix to DistanceDictionary done

--- TSPSolver/TSPSolver/TSPSolver/Services/TspService.cs
+++ TSPSolver/TSPSolver/TSPSolver/Services/TspService.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using TSPSolver.Model;
 
 namespace TSPSolver.Services
@@ -6,10 +7,37 @@ namespace TSPSolver.Services
    public class TspService
    {
       private Dictionary<Address, Dictionary<Address, double>> _distanceMatrix;
+      private List<Address> _addresses;
       private DistanceProvider distanceProvider= new DistanceProvider();
       public TspService(List<Address> addresses)
       {
-         AdjacencyMatrix matrix = distanceProvider.GetDistancesAsync(addresses).Result;
+         _addresses = addresses;
+         AdjacencyMatrix adjacencyMatrix = GetDistancesFromGoogleApi(_addresses);
+         Dictionary<Address, Dictionary<Address, double>> distanceMatrix = parseAdjacencyMatrixToDistanceMatrix(adjacencyMatrix);
+      }
+
+      private AdjacencyMatrix GetDistancesFromGoogleApi(List<Address> addresses)
+      {
+         return distanceProvider.GetDistancesAsync(addresses).Result;
+      }
+
+      private Dictionary<Address, Dictionary<Address, double>> parseAdjacencyMatrixToDistanceMatrix(AdjacencyMatrix matrix)
+      {
+         Dictionary<Address, Dictionary<Address, double>> distancesMatrix = new Dictionary<Address, Dictionary<Address, double>>();
+
+         for (int i = 0; i < _addresses.Count; i++)
+         {
+            Dictionary<Address, double> tempMatrix = new Dictionary<Address, double>();
+            for (int j = 0; j < _addresses.Count; j++)
+            {
+               if (i != j)
+               {
+                  tempMatrix.Add(_addresses.ElementAt(j), matrix.Rows[i].elements[j].distance.value);
+               }
+            }
+            distancesMatrix.Add(_addresses.ElementAt(i), tempMatrix);
+         }
+         return distancesMatrix;
       }
    }
 }
