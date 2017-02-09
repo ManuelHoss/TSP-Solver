84efcac5202b2120265e766dbac13ec63c55feef
Manuel Hoss <manuel.hoss@hotmail.de>
2017-02-09 14:36:40 +0100

    Readdedd Dynamic Alg code

--- TSPSolver/TSPSolver/TSPSolver/TSP Algorithms/DynamicProgramming/TspSolver_DynamicProgramming.cs
+++ TSPSolver/TSPSolver/TSPSolver/TSP Algorithms/DynamicProgramming/TspSolver_DynamicProgramming.cs
@@ -7,7 +7,40 @@ namespace TSPSolver.TSP_Algorithms.DynamicProgramming
 {
    class TspSolver_DynamicProgramming : ITspSolver
    {
+      private int length;
+      private List<Address> adresses;
+      private Dictionary<Address, Dictionary<Address, double>> distanceMatrix;
+
+      private List<Dote> allDotes;
+
       public Route CalculateShortestRoute(Dictionary<Address, Dictionary<Address, double>> distanceMatrix, List<Address> addresses, Address depotAddress)
+      {
+         length = adresses.Count;
+         this.adresses = adresses;
+         this.distanceMatrix = distanceMatrix;
+         findprecedence();
+         builtGraph();
+         return null;
+      }
+
+      private void findprecedence()
+      {
+         return;
+         throw new NotImplementedException("not done jet");
+      }
+
+      private void builtGraph()
+      {
+         List<Dote> currentDotes = new List<Dote>();
+         currentDotes.Add(new Dote());
+         for (int i = 0; i <= length; i++)
+         {
+            currentDotes = makeNewStep(currentDotes);
+
+         }
+      }
+
+      private List<Dote> makeNewStep(List<Dote> currentDotes)
       {
          throw new NotImplementedException();
       }
