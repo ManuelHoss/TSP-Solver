6e0886185d78fafff9d1887d6ec39a146b79081a
David <david.hacka@gmx.de>
2017-02-09 14:30:38 +0100

    beginning

--- TSPSolver/TSPSolver/TSPSolver/TSP Algorithms/DynamicProgramming/TspSolver_DynamicProgramming.cs
+++ TSPSolver/TSPSolver/TSPSolver/TSP Algorithms/DynamicProgramming/TspSolver_DynamicProgramming.cs
@@ -10,9 +10,42 @@ namespace TSPSolver.TSP_Algorithms.DynamicProgramming
 {
    class TspSolver_DynamicProgramming : ITspSolver
    {
-      public Route CalculateShortestRoute(Dictionary<Address, Dictionary<Address, double>> distanceMatrix, List<Address> addresses)
+        private int length;
+        private List<Address> adresses;
+        private Dictionary<Address, Dictionary<Address, double>> distanceMatrix;
+
+        private List<Dote> allDotes;
+
+        public Route CalculateShortestRoute(Dictionary<Address, Dictionary<Address, double>> distanceMatrix, List<Address> addresses)
       {
-         throw new NotImplementedException();
+            length = adresses.Count;
+            this.adresses = adresses;
+            this.distanceMatrix = distanceMatrix;
+            findprecedence();
+            builtGraph();
+            return null;
       }
-   }
+
+        private void findprecedence()
+        {
+            return;
+            throw new NotImplementedException("not done jet");
+        }
+
+        private void builtGraph()
+        {
+            List<Dote> currentDotes = new List<Dote>();
+            currentDotes.Add(new Dote());
+            for (int i = 0; i <= length; i++)
+            {
+                currentDotes = makeNewStep(currentDotes);
+
+            }
+        }
+
+        private List<Dote> makeNewStep(List<Dote> currentDotes)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
